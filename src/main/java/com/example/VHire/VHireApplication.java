0866663bf3f8e2579d220a99b1c6df76d3c5128f
Sachin-2k05git config --global user.email sachinsharm2005@gmail.com <sachinsharm2005@gmail.com>
2025-12-20 20:06:58 +0530

    initial commit

---
+++ src/main/java/com/example/VHire/VHireApplication.java
@@ -0,0 +1,13 @@
+package com.example.VHire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VHireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VHire/VHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VHire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
